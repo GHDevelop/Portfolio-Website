1af92ad320a2fa56e96cde901bd5489065339a33
CTOBN <gappledevmail@gmail.com>
2021-01-20 20:14:24 -0700

    Implemented basic toolbar to test routing.

--- portfolio-website/src/app/app-routing.module.ts
+++ portfolio-website/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { fromEventPattern } from 'rxjs';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'home-page', component: HomePageComponent },
+  { path: 'about-page', component: AboutPageComponent },
+  
+  { path: '', redirectTo: '/home-page', pathMatch: 'full' }
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- portfolio-website/src/app/app.module.ts
+++ portfolio-website/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule,
